05a956ac2976b170c312bc36e22a0897ad53b179
Alex Fallenstedt <a.fallenstedt@gmail.com>
2016-04-11 09:03:40 -0700

    establish basic routing in app file

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,11 +1,18 @@
 import { Component } from 'angular2/core';
+import {RouteConfig} from 'angular2/router';
+import {ROUTER_DIRECTIVES} from 'angular2/router';
 
 @Component({
   selector: 'my-app',
   template: `
-    
-  `
+
+  `,
+    directives: [ROUTER_DIRECTIVES]
 })
+
+@RouteConfig([
+  
+])
 export class AppComponent {
 
 }
