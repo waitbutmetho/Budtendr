b32635df8b34dac5c5b5f186ea26e280e74017b6
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-04-11 13:28:24 -0800

    Change to hash location strategy to fix Cannot GET errors

--- app/main.ts
+++ app/main.ts
@@ -2,8 +2,9 @@ import { bootstrap }    from 'angular2/platform/browser';
 import { AppComponent } from './app.component';
 import { HTTP_PROVIDERS } from 'angular2/http';
 import { DataService } from './data.service';
-import { ROUTER_PROVIDERS } from "angular2/router";
+import { bind } from 'angular2/core';
+import { ROUTER_PROVIDERS, LocationStrategy, HashLocationStrategy } from "angular2/router";
 // import { COMMON_DIRECTIVES } from 'angular2/platform/browser';
 import 'rxjs/Rx';
 
-bootstrap(AppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS, DataService]);
+bootstrap(AppComponent, [HTTP_PROVIDERS, ROUTER_PROVIDERS, bind(LocationStrategy).toClass(HashLocationStrategy), DataService]);
