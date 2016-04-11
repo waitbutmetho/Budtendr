f386e43f8a01a33fac89d68eb8854751ef3beb69
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-04-11 13:38:52 -0800

    update data service to get strains

--- app/data.service.ts
+++ app/data.service.ts
@@ -2,17 +2,23 @@ import { Injectable } from 'angular2/core';
 import { Http, Response } from 'angular2/http';
 import { Observable } from 'rxjs/Observable';
 
+var baseURL = "http://localhost/budtendr/";
+
 @Injectable()
 export class DataService {
   constructor(private _http: Http) {
 
   }
   getData() {
-    return this._http.get('http://localhost/budtendr/request.php')
+    return this._http.get(baseURL + 'request.php')
       .map(res => res.json());
   }
   handleError(error: Response) {
     console.error( error );
     return Observable.throw(error.json().error || "Server Error");
+  }
+  getStrains() {
+    return this._http.get(baseURL + 'strainlist.php')
+      .map(res => res.json());
   }
 }
