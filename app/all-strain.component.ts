7204fd1e4fb5869e74b5f83549e1864431f3af3a
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-04-11 13:30:12 -0800

    repeating strains.

--- app/all-strain.component.ts
+++ app/all-strain.component.ts
@@ -1,11 +1,21 @@
-import { Component } from 'angular2/core';
-
+import { Component, OnInit } from 'angular2/core';
+import { StrainViewComponent } from './strain-view.component';
 
 @Component({
-    template: `all-strain`,
+    directives:[StrainViewComponent],
+    template: `
+      <h1>Strains:</h1>
+      <strain-view *ngFor="#strain of strains" [strain]="currentStrain">
+
+      </strain-view>
+    `,
 
 })
 
-export class AllStrainComponent{
+export class AllStrainComponent implements OnInit{
+  ngOnInit() {
+    this.strains = [1,2,3,4];
+  }
+  strains = [];
 
 }
