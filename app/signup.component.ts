f83fba218e0467489f2d8bc3da6a7ef74f8efedc
-g <-g>
2016-04-11 13:22:32 -0700

    add form to signup route

--- app/signup.component.ts
+++ app/signup.component.ts
@@ -2,7 +2,32 @@ import { Component } from 'angular2/core';
 
 
 @Component({
-    template: `Sign Up`,
+    template: `
+      <div class="container">
+        <form class="form-horizontal">
+          <div class="form-group">
+            <label class="control-label col-sm-2" for="username">Username</label>
+            <div class="col-sm-10">
+                  <input type="text" class="form-control" id="username">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-sm-2" for="password">Password</label>
+            <div class="col-sm-10">
+                  <input type="text" class="form-control" id="password">
+            </div>
+          </div>
+          <div class="form-group">
+            <label class="control-label col-sm-2" for="confirm-password">Confirm Password</label>
+            <div class="col-sm-10">
+                  <input type="text" class="form-control" id="confirm-password">
+            </div>
+          </div>
+              <button class="btn btn-success">Submit</button>
+        </form>
+      </div>
+
+    `,
 })
 
 export class SignUpComponent{
