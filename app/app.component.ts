919f641f7423427216589a7957261ef66caf3454
-g <-g>
2016-04-11 09:11:14 -0700

    save progress

--- app/app.component.ts
+++ app/app.component.ts
@@ -5,13 +5,21 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 @Component({
   selector: 'my-app',
   template: `
-
+  <header>
+    <ul>
+      <li>Home</li>
+      <li>Search</li>
+      <li>Dispensary</li>
+      <li>User</li>
+      <li>Sign Up</li>
+    </ul>
+  </header>
   `,
     directives: [ROUTER_DIRECTIVES]
 })
 
 @RouteConfig([
-  
+
 ])
 export class AppComponent {
 
