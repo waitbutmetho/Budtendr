96cb50fbe1a34e35996e60185ba9eeba217a2de4
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-04-12 10:45:33 -0800

    working post between service and api

--- app/data.service.ts
+++ app/data.service.ts
@@ -1,8 +1,9 @@
 import { Injectable } from 'angular2/core';
-import { Http, Response } from 'angular2/http';
+import { Http, Response, Headers } from 'angular2/http';
 import { Observable } from 'rxjs/Observable';
 
-var baseURL = "http://nicholasjensenhay.com/budtendrapi/";
+var baseURL = 'http://localhost/budtendr/';
+// var baseURL = "http://nicholasjensenhay.com/budtendrapi/";
 
 @Injectable()
 export class DataService {
@@ -12,6 +13,21 @@ export class DataService {
   handleError(error: Response) {
     console.error( error );
     return Observable.throw(error.json().error || "Server Error");
+  }
+  makeData(keys, values) {
+    var output = "";
+    if(keys.length === values.length) {
+      keys.forEach(function(key, index) {
+        output += key+"="+values[index]+"&";
+      });
+      output = output.substr(0, output.length-1);
+    }
+    return output;
+  }
+  postRequest(url, data) {
+    var headers = new Headers();
+    headers.append('Content-Type', 'application/x-www-form-urlencoded');
+    return this._http.post(baseURL+url, data, { headers: headers}).map(res => res.json());
   }
   getStrains(page=0) {
     return this._http.get(baseURL + 'strainlist.php?page='+page)
