169087254290ff49efc2de8908702b0596f4d121
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-04-12 09:07:06 -0800

    Add dispensary id routing

--- app/dispensary.component.ts
+++ app/dispensary.component.ts
@@ -1,10 +1,18 @@
 import { Component } from 'angular2/core';
-
+import { DataService } from './data.service';
+import { RouteParams } from 'angular2/router';
 
 @Component({
-    template: `Dispensary Page`,
+  providers: [DataService],
+    template: `<h1>{{dispensary.name}}</h1>`,
 })
 
 export class DispensaryComponent{
-
+  dispensary = [];
+  constructor(params: RouteParams, private _dataService: DataService) {
+    this.getDispensary(params.get('id'));
+  }
+  getDispensary(id) {
+    this._dataService.getDispensary(id).subscribe(resp => this.dispensary = resp.dispensary);
+  }
 }
