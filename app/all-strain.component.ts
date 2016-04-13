82c0c8885300b13b6d9a4f933431ad8a8834f0fb
-g <-g>
2016-04-13 11:01:32 -0700

    show pages on all strain

--- app/all-strain.component.ts
+++ app/all-strain.component.ts
@@ -12,8 +12,10 @@ import { DataService } from './data.service';
 
 export class AllStrainComponent implements OnInit{
   strains = [];
-  maxpage=0;
+  maxpage = 0;
   page = 0;
+  count= 0;
+  pages = [];
   ngOnInit() {
 
   }
@@ -21,11 +23,12 @@ export class AllStrainComponent implements OnInit{
     this.getStrains();
   }
   getStrains() {
-    var self = this;
+    var self = this;//isolate scope
     this._dataService.getStrains(this.page).subscribe(function(strains){
       self.strains = strains.strains;
       self.maxpage = strains.maxpage;
-      console.log(self.maxpage);
+      console.log('maxpage is', self.maxpage);
+      self.showPage();
     });
   }
   nextPage() {
@@ -41,4 +44,14 @@ export class AllStrainComponent implements OnInit{
     this.getStrains();
 
   }
+  showPage(){
+    while(this.count <= this.maxpage) {
+      console.log('current count', this.count);
+      this.count = this.count += 1;
+      this.pages.push(this.count);//push count into array called pages
+      console.log('var pages', this.pages);
+    }
+
+  }
+
 }
