f813d0ac3e9b19b5c231bd68083aa9dc605b0215
waitbutmetho <silversevenfox@yahoo.com>
2016-04-14 14:23:51 -0700

    remove user component and user template w/ imports and paths

--- app/app.component.ts
+++ app/app.component.ts
@@ -6,7 +6,6 @@ import {ROUTER_DIRECTIVES} from 'angular2/router';
 import {SearchComponent} from './search.component';
 import {DispensaryComponent} from './dispensary.component';
 import {DispensaryAdminComponent} from './dispensary-admin.component';
-import {UserComponent} from './user.component';
 import {SignUpComponent} from './signup.component';
 import {AddDispensaryComponent} from './add-dispensary.component';
 import {AddStrainComponent} from './add-strain.component';
@@ -28,7 +27,6 @@ import { EditStrainComponent } from './edit-strain.component';
   {path: '/search', name: 'Search', component: SearchComponent  },
   {path: '/dispensary/:id', name: 'Dispensary', component: DispensaryComponent },
   {path: '/dispensaryAdmin/:id', name: 'DispensaryAdmin', component: DispensaryAdminComponent},
-  {path: '/user', name: 'User', component: UserComponent },
   {path: '/sign-up', name: 'SignUp', component: SignUpComponent },
   {path: '/add-dispensary', name: 'AddDispensary', component: AddDispensaryComponent },
   {path: '/add-strain', name: 'AddStrain', component: AddStrainComponent },
@@ -50,7 +48,6 @@ export class AppComponent {
     var self = this;
     this._dataService.login(form.username, form.password).subscribe(function(res) {
       self.user = res.user;
-      self._router.navigate(['User']);
       $('#login-overlay').modal('hide');
     });
   }
