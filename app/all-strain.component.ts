2dcb497c7f9381e98fa5acce549832e3227b4906
-g <-g>
2016-04-13 17:18:03 -0700

    add buttons for filters

--- app/all-strain.component.ts
+++ app/all-strain.component.ts
@@ -33,42 +33,4 @@ export class AllStrainComponent implements OnInit{
       // self.showPage();
     });
   }
-
-  sortName(){
-    console.log('sort by name');
-    var self= this;
-    this._dataService.getStrains(this.page).subscribe(function(strains){
-      self.strains = strains.strains;
-      console.log(self.strains);
-    });
-  }
-
-  // nextPage() {
-  //   this.incrementPage(1);
-  // }
-  // prevPage() {
-  //   this.incrementPage(-1);
-  // }
-  // incrementPage(num) {
-  //   this.page = this.page + num;
-  //   if(this.page < 0) this.page = 0;
-  //   if(this.page > this.maxpage) this.page = this.maxpage;
-  //   this.getStrains();
-  // }
-  //
-  // goToPage(num){
-  //   this.page = num;
-  //   this.getStrains();
-  // }
-  //
-  // showPage(){
-  //   while(this.count < this.maxpage) {
-  //     this.count = this.count += 1;
-  //     this.pages.push(this.count);//push count into array called pages
-  //
-  //   }
-  //   if (this.pages.length > 5) {
-  //     this.pages
-  //   }
-  // }
 }
