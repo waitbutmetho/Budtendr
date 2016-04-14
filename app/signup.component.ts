9b7f7c07937c7c273697758935929c6241e0bf99
michaeldada <mail.michaeldada@gmail.com>
2016-04-13 17:37:26 -0700

    add styling to signup form

--- app/signup.component.ts
+++ app/signup.component.ts
@@ -4,42 +4,7 @@ import { DataService } from './data.service';
 
 @Component({
   providers: [DataService],
-    template: `
-      <div class="container">
-        <form (ngSubmit)="onSubmit(form.value)" #form="ngForm" class="form-horizontal">
-        <h3>Sign up for a Budtendr account</h3>
-          <div class="form-group">
-            <label class="control-label col-sm-2" for="username">Username</label>
-            <div class="col-sm-10">
-                  <input ngControl="username" #username="ngForm" type="text" class="form-control" id="username" required>
-                  <div class="alert alert-danger" *ngIf=" username.touched && !username.valid">Username is required.</div>
-
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="control-label col-sm-2" for="email">Email</label>
-            <div class="col-sm-10">
-                  <input ngControl="email" #email="ngForm" type="text" class="form-control" id="email" required>
-                  <div class="alert alert-danger" *ngIf="email.touched && !email.valid">Email is required.</div>
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="control-label col-sm-2" for="password">Password</label>
-            <div class="col-sm-10">
-                  <input  ngControl="password" #password="ngForm" type="text" class="form-control" id="password">
-            </div>
-          </div>
-          <div class="form-group">
-            <label class="control-label col-sm-2" for="confirm-password">Confirm Password</label>
-            <div class="col-sm-10">
-                  <input ngControl="confirm-password" #confirmPassword="ngForm" type="text" class="form-control" id="confirm-password">
-            </div>
-          </div>
-              <button class="btn btn-success">Submit</button>
-        </form>
-        <button (click)="onNavigate()" class="btn btn-primary">I'm a Dispensary Owner</button>
-      </div>
-    `,
+    templateUrl: "app/templates/signup.component.html" ,
 })
 export class SignUpComponent{
   onSubmit(form) {
