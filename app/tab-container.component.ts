f96b29f659287f25b74c02fcf2c002a1140ce2b4
Nicholas Jensen-Hay <nicholasjensenhay@gmail.com>
2016-04-15 08:06:21 -0800

    Dispensary menu sorting

--- app/tab-container.component.ts
+++ app/tab-container.component.ts
@@ -1,4 +1,4 @@
-import {Component} from 'angular2/core'
+import {Component, EventEmitter} from 'angular2/core'
 
 import {TabsComponent} from './tabs.component';
 import {TabComponent} from './tab.component';
@@ -7,15 +7,23 @@ import {DispensaryMenuComponent} from './dispensary-menu.component';
 
 @Component({
   inputs: ["strains", "map"],
+  outputs: ['sortBy'],
   selector: 'tab-container',
   template: `
     <tabs>
-      <tab [tabName]="'Menu'"><dispensary-menu [strains]="strains"></dispensary-menu></tab>
+      <tab [tabName]="'Menu'"><dispensary-menu (sortBy)="sort($event)" [strains]="strains"></dispensary-menu></tab>
       <tab [tabName]="'Map'"><dispensary-details [map]="map"></dispensary-details></tab>
     </tabs>
   `,
   directives: [TabsComponent, TabComponent, DispensaryDetailsComponent, DispensaryMenuComponent]
 })
 export class TabContainerComponent {
-
+  sortBy: EventEmitter<string>;
+  constructor() {
+    this.sortBy = new EventEmitter();
+  }
+  sort(sortBy) {
+    console.log('tabcon', sortBy);
+    this.sortBy.emit(sortBy);
+  }
 }
